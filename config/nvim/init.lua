0541ec9eadb7917d8554a37ff85d36e4840817f9
Scott Wysocki <swysocki@gmail.com>
2022-07-31 19:53:48 -0400

    install treesitter langs automatically

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -73,6 +73,9 @@ cmp.setup({
 
 
 require('nvim-treesitter.configs').setup {
+    ensure_installed = { "python", "yaml", "bash", "lua" },
+    sync_install = false,
+    auto_install = true,
     highlight = {enable = true, additional_vim_regex_highlighting = false},
     indent = {enable = true}
 }
