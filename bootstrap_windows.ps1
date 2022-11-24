a140e9a25a241cd339c5dd80c8a74b4d5c277136
Scott Wysocki <swysocki@gmail.com>
2022-11-24 13:43:35 -0500

    wip: create a windows setup script

---
+++ bootstrap_windows.ps1
@@ -0,0 +1,32 @@
+# install utilities needed by my dotfiles
+
+winget install Git.Git
+winget install WezTerm
+winget install Vim
+
+function Append-Path {
+	param($NewPath)
+	$userpath = [System.Environment]::GetEnvironmentVariable("PATH","USER")
+	$userpath = $userpath + ";$NewPath"
+	[System.Environment]::SetEnvironmentVariable("PATH",$userpath,"USER")
+}
+
+# clone my dotfiles
+git clone https://github.com/swysocki/dotfiles.git $HOME/devel/repos/dotfiles
+git submodule update --init
+
+# create links
+New-Item -Path $HOME\.wezterm.lua -ItemType HardLink -V $HOME\Devel\Repos\dotfiles\wezterm.lua
+New-Item -Path $HOME\_vimrc -ItemType HardLink -V $HOME\Devel\Repos\dotfiles\vimrc
+New-Item -Path $HOME\vimfiles -ItemType Junction -V $HOME\Devel\Repos\dotfiles\vim
+
+# install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+$curDir=$PWD.path
+cd $HOME\.fzf
+.\install.ps1
+cd $curDir
+
+
+# $myPath=$(Join-Path $HOME -ChildPath ".fzf\bin\")
+# Append-Path $myPath
