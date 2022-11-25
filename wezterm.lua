4b810c03b51932fdc73f3fea651a13e771ef17bb
Scott Wysocki <swysocki@gmail.com>
2022-11-24 20:31:50 -0500

    wezterm: add windows specific configs

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,14 @@
-return {
-  color_scheme = "Gruvbox Dark",
-  font_size = 10.0,
+local wezterm = require("wezterm")
+
+local config = {
+  color_scheme = "Nord (base16)",
+  font_size = 12.0,
   enable_tab_bar = true,
 }
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+  config.font_size = 10.0
+  config.default_prog = {'powershell'}
+end
+
+return config
