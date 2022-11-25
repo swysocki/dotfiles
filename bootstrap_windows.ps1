d0741d7916823bb3e06f3c220cf51e4ac9ec5548
Scott Wysocki <swysocki@gmail.com>
2022-11-24 21:10:46 -0500

    windows: check for existing files

--- bootstrap_windows.ps1
+++ bootstrap_windows.ps1
@@ -3,24 +3,29 @@ $dotfilePath="$HOME\devel\repos\dotfiles"
 # install utilities needed by my dotfiles
 winget install Git.Git
 winget install WezTerm
-winget install Vim
+winget install vim.vim
 
 # clone my dotfiles
-git clone https://github.com/swysocki/dotfiles.git $dotfilePath
-git submodule update --init
+if (!(Test-Path "$dotfilePath")) {
+	git clone https://github.com/swysocki/dotfiles.git $dotfilePath
+	git submodule update --init
+}
 
 # create links
 New-Item -Path $HOME\.wezterm.lua -ItemType HardLink -Value "$dotfilePath\wezterm.lua" -Force
 New-Item -Path $HOME\_vimrc -ItemType HardLink -Value "$dotfilePath\vimrc" -Force
-New-Item -Path $HOME\vimfiles -ItemType Junction -Value "$dotfilePath\vim" -Force
+if (!(Test-Path "$dotfilePath\vim")) {
+	New-Item -Path $HOME\vimfiles -ItemType Junction -Value "$dotfilePath\vim"
+}
 
 # Copy Powershell Profile
-Copy-Item "$dotfilePath\Microsoft.PowerShell_profile.ps1" $PROFILE
+Copy-Item "$dotfilePath\Microsoft.PowerShell_profile.ps1" $PROFILE -Force
 
 # install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git $HOME\.fzf
-$curDir=$PWD.path
-cd $HOME\.fzf
-.\install.ps1
-cd $curDir
-
+if (!(Test-Path "$HOME\.fzf")) {
+	git clone --depth 1 https://github.com/junegunn/fzf.git $HOME\.fzf
+	$curDir=$PWD.path
+	cd $HOME\.fzf
+	.\install.ps1
+	cd $curDir
+}
