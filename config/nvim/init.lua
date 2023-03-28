8b8f503082fb33d49f86ccd21dec9d39c922515d
Scott Wysocki <swysocki@gmail.com>
2023-03-28 10:41:14 -0400

    nvim: don't use treesitter indent

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -77,7 +77,7 @@ require('nvim-treesitter.configs').setup {
     sync_install = false,
     auto_install = true,
     highlight = {enable = true, additional_vim_regex_highlighting = false},
-    indent = {enable = true}
+    -- indent = {enable = true}
 }
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
